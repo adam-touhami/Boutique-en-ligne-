ff6180f42ba1f0d284ec4da6641531be978158e7
adam-touhami <55872643+adam-touhami@users.noreply.github.com>
2020-10-26 14:53:39 +0100

    Update boutique.sql

--- Database/boutique.sql
+++ Database/boutique.sql
@@ -71,9 +71,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `contact`, `city`, `address`) VALUES
-(1, 'Sajal', 'sajal.agrawal1997@gmail.com', '57f231b1ec41dc6641270cb09a56f897', '8899889988', 'Indore', '100 palace colony, Indore'),
-(2, 'Ram', 'ram1234@xyz.com', '57f231b1ec41dc6641270cb09a56f897', '8899889989', 'Pune', '100 palace colony, Pune'),
-(3, 'Shyam', 'shyam@xyz.com', '57f231b1ec41dc6641270cb09a56f897', '8899889990', 'Bangalore', '100 palace colony, Bangalore');
+(1, 'Baki', 'baki@gmail.com', '57f231b1ec41dc6641270cb09a56f897', '8899889988', 'test', 'test'),
+(2, 'Adam', 'adam@xyz.com', '57f231b1ec41dc6641270cb09a56f897', '8899889989', 'test', 'test'),
 
 -- --------------------------------------------------------
 
